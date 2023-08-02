b1083b5581a572e0a7686a6d48ad3a4deb6598ce
Wdjway <djway20@gmail.com>
2023-08-02 09:54:33 -0500

    Actividad de Javascript

---
+++ Ejercicios.js
@@ -0,0 +1,40 @@
+// Ejercicio 1: Area 
+
+// let base = prompt("Ingrese base")
+// let altura = prompt("Ingrese altura")
+// let area = (parseFloat(base)* parseFloat(altura) / 2)
+
+
+// Ejercicio 2: Conversion USD Una empresa importadora desea determinar cuántos dólares puede adquirir
+// con determinada cantidad de pesos. Realiza el algoritmo para tal fin.
+
+// let USD = 4023
+// let pesos = prompt("Ingrese su valor en pesos")
+
+// let Resultado =(parseFloat(pesos) / USD)
+
+// console.log (Resultado)
+
+// Ejercicio 3: Una empresa que contrata personal requiere determinar la edad de las
+// personas que solicitan trabajo, pero cuando se les realiza la entrevista sólo
+// se les pregunta el año en que nacieron. Realiza el algoritmo para solucionar
+// este problema.
+
+// let año_nacimiento = prompt("Ingrese su año de nacimiento")
+// let año = 2023
+
+// let Resultado2 = (parseFloat(año_nacimiento)-(año))
+
+// console.log (Resultado2)
+
+// Un estacionamiento requiere determinar el cobro que debe aplicar a las
+// personas que lo utilizan. Considera que el cobro es con base en las horas
+// que lo disponen y que las fracciones de hora se toman como completas,
+// realiza el algoritmo que permita determinar el cobro.
+
+// let precio_estacionamiento = 3500
+// let hora = prompt("ingrese numero de horas")
+// let minuto = 0 
+
+
+
